ba3c35661dfd931d80e8b7918d611444b3a30ac0
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-07-04 09:16:54 +0600

    product creation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { ProductRoutes } from './app/modules/product/product.routes';
 
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use('/api', ProductRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`Server health is good and running well`);
--- src/app/modules/product/product.schema.ts
+++ src/app/modules/product/product.schema.ts
@@ -1,28 +1,28 @@
-import mongoose, { Schema } from 'mongoose';
-import { Inventory, Product, Variant } from './product.interface';
+// import mongoose, { Schema } from 'mongoose';
+// import { Inventory, Product, Variant } from './product.interface';
 
-// Define the structure for a Variant
-const VariantSchema = new Schema<Variant>({
-  type: { type: String, required: true },
-  value: { type: String, required: true }
-});
+// // Define the structure for a Variant
+// const VariantSchema = new Schema<Variant>({
+//   type: { type: String, required: true },
+//   value: { type: String, required: true }
+// });
 
-// Define the structure for Inventory
-const InventorySchema = new Schema<Inventory>({
-  quantity: { type: Number, required: true },
-  inStock: { type: Boolean, required: true }
-});
+// // Define the structure for Inventory
+// const InventorySchema = new Schema<Inventory>({
+//   quantity: { type: Number, required: true },
+//   inStock: { type: Boolean, required: true }
+// });
 
-// Define the structure for the main Product
-const ProductSchema = new Schema<Product>({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: String, required: true },
-  tags: { type: [String], required: true },
-  variants: { type: [VariantSchema], required: true },
-  inventory: { type: InventorySchema, required: true }
-});
+// // Define the structure for the main Product
+// const ProductSchema = new Schema<Product>({
+//   name: { type: String, required: true },
+//   description: { type: String, required: true },
+//   price: { type: Number, required: true },
+//   category: { type: String, required: true },
+//   tags: { type: [String], required: true },
+//   variants: { type: [VariantSchema], required: true },
+//   inventory: { type: InventorySchema, required: true }
+// });
 
-// Create the Mongoose model
-export const ProductModel = mongoose.model<Product>('Product', ProductSchema);
+// // Create the Mongoose model
+// export const ProductModel = mongoose.model<Product>('Product', ProductSchema);
